a987e35e025d30684f10edc7be61e53d5ee5d921
Carlos Abarca <soporte@compuelec.cl>
2023-07-25 17:47:00 -0400

    + Update: swagger integration to document the api

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,28 +1,62 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class CreateUser {
   _id: string;
+
+  @ApiProperty()
   username: string;
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   lastName: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
+
   isAdmin: boolean;
+
   isVerified: boolean;
+
   isActive: boolean;
+
   token: string;
+
+  @ApiPropertyOptional()
   avatar: string;
 }
 export class UpdateUser {
+  @ApiPropertyOptional()
   username?: string;
+
+  @ApiPropertyOptional()
   name?: string;
+
+  @ApiPropertyOptional()
   lastName?: string;
+
+  @ApiPropertyOptional()
   password?: string;
+
+  @ApiPropertyOptional()
   isAdmin?: boolean;
+
   isVerified?: boolean;
+
   isActive?: boolean;
+
   token?: string;
+
+  @ApiPropertyOptional()
   avatar?: string;
 }
 export class LoginDto {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
