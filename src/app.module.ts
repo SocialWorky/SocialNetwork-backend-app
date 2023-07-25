94b33b12eb298eecee173ef974bac3d2b980c187
Carlos Abarca <soporte@compuelec.cl>
2023-07-25 09:23:19 -0400

    - Update: Added message token has expired

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER } from '@nestjs/core';
+import { JwtExpiredFilter } from './filters/jwt-expired.filter';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -15,12 +15,16 @@ import { AuthModule } from './auth/auth.module';
       password: 'Losbar191184@',
       database: 'bside',
       autoLoadEntities: true,
-      synchronize: true, // Solo utilizar synchronize en desarrollo (crea automáticamente las tablas, no en producción)
+      synchronize: true, //Only use synchronize in development (automatically creates tables), not in production.
     }),
     UsersModule,
     AuthModule,
   ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: JwtExpiredFilter,
+    },
+  ],
 })
 export class AppModule {}
