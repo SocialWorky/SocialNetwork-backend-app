8e6b83b63aa2010e35ad9111123076a8a1ce9ab9
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-02 01:22:39 -0400

    Update email templates and add forgot password functionality

--- src/modules/mails/entities/mail.entity.ts
+++ src/modules/mails/entities/mail.entity.ts
@@ -1,5 +1,21 @@
 export class MailDataValidate {
-  url: string;
+  token?: string;
+  email?: string;
+  password?: string;
+  url?: string;
+  subject: string;
+  title: string;
+  greet: string;
+  message: string;
+  subMessage: string;
+  buttonMessage: string;
+}
+
+export interface MailerDataValidate {
+  token?: string;
+  email?: string;
+  password?: string;
+  url?: string;
   subject: string;
   title: string;
   greet: string;
